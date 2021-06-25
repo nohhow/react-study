45af72f887c09e8b574ae5f825ff9d1f2fd3983b
nohhow <xksrma97@gmail.com>
2021-06-25 19:22:22 +0900

    오류 수정

--- src/App.js
+++ src/App.js
@@ -27,6 +27,23 @@ class App extends Component {
     });
   }
 
+  handleUpdate = (id, data) => {
+    const {information} = this.state;
+    this.setState({
+      information : information.map(
+        info => {
+          if (info.id === id) {
+            return {
+              id,
+              ...data,
+            };
+          }
+          return info;
+        }
+      )
+    });
+  }
+
   render() {
     return (
       <div>
@@ -34,6 +51,7 @@ class App extends Component {
         <PhoneInfoList 
           data={this.state.information}
           onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate}
           />
       </div>
     );
--- src/components/PhoneInfoList.js
+++ src/components/PhoneInfoList.js
@@ -6,10 +6,11 @@ class PhoneInfoList extends Component {
         data : []
     }
     render() {
-        const { data, onRemove } = this.props;
+        const { data, onRemove, onUpdate } = this.props;
         const list = data.map(
             info => (
                 <PhoneInfo 
+                    onUpdate={onUpdate}
                     onRemove={onRemove} 
                     info={info} 
                     key={info.id}
